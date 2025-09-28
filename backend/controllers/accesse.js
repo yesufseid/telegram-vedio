5d88600cee8bbc68339ee26fe59352d884cdf217
Seid Yesuf <seidyesuf750@gmail.com>
2025-09-28 22:30:05 +0300

    remove lieasner

--- backend/controllers/accesse.js
+++ backend/controllers/accesse.js
@@ -7,9 +7,6 @@ const asyncWarapper=require("../middleware/async")
 
 const getAccesse=async(req,res)=>{
   console.log("hi"); 
-  const data=listener 
-    console.log(data);
-    // res.json(JSON.stringify(data))
   };
 
 
